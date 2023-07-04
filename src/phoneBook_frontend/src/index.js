b04be0aad8940c440273cd6e0ed4e0b12c453aaa
Mukunde Brisa <brisamukunde1@gmail.com>
2023-07-04 15:02:00 +0300

    frontend

--- src/phoneBook_frontend/src/index.js
+++ src/phoneBook_frontend/src/index.js
@@ -1,19 +1,49 @@
-import { phoneBook_backend } from "../../declarations/phoneBook_backend";
+ import { phoneBook_backend } from "../../declarations/phoneBook_backend";
 
-document.querySelector("form").addEventListener("submit", async (e) => {
-  e.preventDefault();
-  const button = e.target.querySelector("button");
+const contactForm = document.getElementById('contactForm');
+ const cardContainer = document.getElementById('cardContainer');
 
-  const name = document.getElementById("name").value.toString();
+    window.addEventListener('load', async(e)=>{
+      displayContacts()
+    })
 
-  button.setAttribute("disabled", true);
+    contactForm.addEventListener('submit',async (e) => {
+      e.preventDefault();
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await phoneBook_backend.greet(name);
+      const name = document.getElementById('name').value;
+      const desc = document.getElementById('description').value;
+      const phone = document.getElementById('number').value;
+      
+      const result = await phoneBook_backend.insert(name,desc,phone);
+      console.log(result);
 
-  button.removeAttribute("disabled");
+      displayContacts();
+      contactForm.reset();
+    });
 
-  document.getElementById("greeting").innerText = greeting;
+    async function displayContacts() {
+      cardContainer.innerHTML = '';
 
-  return false;
-});
+      const results = await phoneBook_backend.getAllContacts()
+
+      for (const contact of results) {
+        const card = document.createElement('div');
+        card.classList.add('card');
+
+        const nameElement = document.createElement('p');
+        nameElement.textContent = `Name: ${contact.name}`;
+
+        const descriptionElement = document.createElement('p');
+        descriptionElement.textContent = `Description: ${contact.desc}`;
+
+        const numberElement = document.createElement('p');
+        numberElement.textContent = `Number: ${contact.phone}`;
+
+        card.appendChild(nameElement);
+        card.appendChild(descriptionElement);
+        card.appendChild(numberElement);
+
+        cardContainer.appendChild(card);
+      }
+    }
+
